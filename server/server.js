69849f7c6bab155917bfa0c79a5e6a5c340c12b5
LikeJasper <williamclark.home@gmail.com>
2015-11-07 12:27:22 +0100

    Get balance now displays user balance in satoshis for access token in settings

--- server/server.js
+++ server/server.js
@@ -2,9 +2,12 @@ Meteor.startup(function () {
   // code to run on server at startup
 });
 
+var PRIMEDICE_API_ROOT = 'https://api.primedice.com/api/';
+var ACCESS_TOKEN = Meteor.settings.access_token;
+
 Meteor.methods({
   getBalance: function (username) {
-    var responseObj = HTTP.call('GET', 'https://api.primedice.com/api/users/' + username);
+    var responseObj = HTTP.call('GET', PRIMEDICE_API_ROOT + 'users/1?access_token=' + ACCESS_TOKEN);
     return responseObj;
   }
 });

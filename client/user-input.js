2765a43fcc4aad2523fb6ffe5c4338a929e77885
LikeJasper <williamclark.home@gmail.com>
2015-11-07 15:55:38 +0100

    Make a bet button now makes a bet with fixed parameters

--- client/user-input.js
+++ client/user-input.js
@@ -1,23 +1,59 @@
-Template.userInput.helpers({
-  getBalance: function () {
-    return Session.get('userBalance') || 0;
-  }
-});
+/*global
+  Template
+*/
 
-Template.userInput.events({
-  'click #get-balance': function (event) {
-    event.preventDefault();
-    var errorMsg = '';
+(function () {
+  'use strict';
 
-    Meteor.call('getBalance', function (error, result) {
-      if (error) {
-        errorMsg = error.message;
-      } else {
-        console.log(result);
-        Session.set('userBalance', Math.floor(result.data.user.profit));
-      }
-    });
+  Template.displayBalance.helpers({
+    getBalance: function () {
+      return Session.get('userBalance') || 0;
+    }
+  });
 
-    $('.error-text').text(errorMsg);
-  }
-});
+  Template.displayBalance.events({
+    'click #get-balance': function (event) {
+      var errorMsg = '';
+
+      Meteor.call('getBalance', function (error, result) {
+        if (error) {
+          errorMsg = error.message;
+        } else {
+          console.log(result);
+          Session.set('userBalance', Math.floor(result.data.user.profit));
+        }
+      });
+
+      $('#balance-area .error-text').text(errorMsg);
+    }
+  });
+
+  Template.makeBet.helpers({
+    getBetTotal: function () {
+      return Session.get('betTotal') || 0;
+    },
+    getWinLoss: function () {
+      return Session.get('winLoss') || 'won';
+    },
+    getWinLossTotal: function () {
+      return Session.get('winLossTotal') || 0;
+    }
+  });
+
+  Template.makeBet.events({
+    'click #make-bet': function (event) {
+      var errorMsg = '';
+
+      Meteor.call('makeBet', function (error, result) {
+        if (error) {
+          errorMsg = error.message;
+        } else {
+          console.log(result);
+        }
+      });
+
+      $('#bet-area .error-text').text(errorMsg);
+    }
+  });
+
+}());
--- server/server.js
+++ server/server.js
@@ -1,12 +1,36 @@
-Meteor.startup(function () {
-  // code to run on server at startup
-});
+/*global
+  Meteor, HTTP
+*/
 
-var PRIMEDICE_API_ROOT = 'https://api.primedice.com/api/';
-var ACCESS_TOKEN = Meteor.settings.access_token;
+(function () {
+  'use strict';
 
-Meteor.methods({
-  getBalance: function () {
-    return HTTP.call('GET', PRIMEDICE_API_ROOT + 'users/1?access_token=' + ACCESS_TOKEN);
-  }
-});
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+
+  var PRIMEDICE_API_ROOT = 'https://api.primedice.com/api/';
+  var ACCESS_TOKEN = Meteor.settings.access_token;
+  var ACCESS_SUFFIX = '?access_token=' + ACCESS_TOKEN;
+
+  Meteor.methods({
+    getBalance: function () {
+      return HTTP.get(PRIMEDICE_API_ROOT + 'users/1' + ACCESS_SUFFIX);
+    },
+    makeBet: function () {
+      try {
+        return HTTP.post(PRIMEDICE_API_ROOT + 'bet' + ACCESS_SUFFIX, {
+          params: {
+            'amount': 1,
+            'target': 50,
+            'condition': '<'
+          }
+        });
+      }
+      catch (err) {
+        return err;
+      }
+    }
+  });
+
+}());

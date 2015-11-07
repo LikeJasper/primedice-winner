a7b2caaf2fa17e1bd1b3d51cd53c4686a6cc7e22
LikeJasper <williamclark.home@gmail.com>
2015-11-07 12:35:11 +0100

    Cleaned up code, removed irrelevant username input

--- client/user-input.js
+++ client/user-input.js
@@ -5,34 +5,19 @@ Template.userInput.helpers({
 });
 
 Template.userInput.events({
-  'submit form': function (event) {
+  'click #get-balance': function (event) {
     event.preventDefault();
+    var errorMsg = '';
 
-    var username = $('[name=username]').val();
-    var $errorBox = $('.error-text');
-
-    if (username) {
-
-      if (username !== Session.get('username')) {
-        Session.set('username', username);
+    Meteor.call('getBalance', function (error, result) {
+      if (error) {
+        errorMsg = error.message;
+      } else {
+        console.log(result);
+        Session.set('userBalance', Math.floor(result.data.user.profit));
       }
+    });
 
-      Meteor.call('getBalance', username, function (error, result) {
-        if (error) {
-          $errorBox.text(error.message);
-        } else {
-          console.log(result);
-          Session.set('userBalance', result.data.user.profit);
-          $errorBox.text('');
-        }
-      });
-
-      // var userBalance = Session.get('userBalance');
-      // var newBalance = typeof(userBalance) === "number" ? userBalance + 1 : 0;
-      // Session.set('userBalance', newBalance);
-
-    } else {
-      $errorBox.text("You need to put in a username");
-    }
+    $('.error-text').text(errorMsg);
   }
 });
--- server/server.js
+++ server/server.js
@@ -6,8 +6,7 @@ var PRIMEDICE_API_ROOT = 'https://api.primedice.com/api/';
 var ACCESS_TOKEN = Meteor.settings.access_token;
 
 Meteor.methods({
-  getBalance: function (username) {
-    var responseObj = HTTP.call('GET', PRIMEDICE_API_ROOT + 'users/1?access_token=' + ACCESS_TOKEN);
-    return responseObj;
+  getBalance: function () {
+    return HTTP.call('GET', PRIMEDICE_API_ROOT + 'users/1?access_token=' + ACCESS_TOKEN);
   }
 });
